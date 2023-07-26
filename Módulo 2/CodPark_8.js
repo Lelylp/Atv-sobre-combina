7d69d2ce2e1efd4b67728959c6b3167c3ffe6ca1
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-07-26 19:16:40 +0000

    add throw error

--- Módulo 2/CodPark_8.js
+++ Módulo 2/CodPark_8.js
@@ -3,12 +3,14 @@ do {
     nome = require('readline-sync').question("Digite seu nome completo: ");
     if (nome.length<15) {
         console.log("Nome muito pequeno");
+        //throw new Error ("Nome muito pequeno");
     }
 } while (nome.length<15);
 do {
     idade = require('readline-sync').questionInt("Digite seu ano de nascismento: ");
     if (idade<=1922 || idade>=2021) {
         console.log("Fora do range");
+        //throw new Error ("Fora do range");
     }
 } while (idade<=1922 || idade>=2021);
 idade = 2022 - idade;

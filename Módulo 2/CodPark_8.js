ebf0ab9782ee17e9037d9faed0d46ed9e594a817
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-07-26 19:37:30 +0000

    add muitas coisas dnv

--- Módulo 2/CodPark_8.js
+++ Módulo 2/CodPark_8.js
@@ -1,18 +1,30 @@
 let nome, idade;
 do {
-    nome = require('readline-sync').question("Digite seu nome completo: ");
-    if (nome.length<15) {
-        console.log("Nome muito pequeno");
-        //throw new Error ("Nome muito pequeno");
+    try {
+        nome = require('readline-sync').question("Digite seu nome completo: ");
+        if (nome.length<15) {
+            throw new Error ("Nome muito pequeno");
+        }
+    } catch (error) {
+        console.log(error);
     }
 } while (nome.length<15);
+
 do {
-    idade = require('readline-sync').questionInt("Digite seu ano de nascismento: ");
-    if (idade<=1922 || idade>=2021) {
-        console.log("Fora do range");
-        //throw new Error ("Fora do range");
+    try {
+        idade = require('readline-sync').question("Digite seu ano de nascismento: ");
+        if (parseInt(idade)<=1922 || parseInt(idade)>=2021) {
+            throw new Error ("Fora do range(só é permitido 1922 a 2021)");
+        }
+        if(isNaN(idade)){
+            throw new Error ("Somente números");
+        }
+    } catch (error) {
+        console.log(error);
     }
-} while (idade<=1922 || idade>=2021);
+} while (parseInt(idade)<=1922 || parseInt(idade)>=2021);
+
+
 idade = 2022 - idade;
 let pessoa = {nome, idade};
 console.log(pessoa);
